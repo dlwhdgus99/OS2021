fcf03cbba429c8348fd2715402cc09e45d8d163e
jjong <2019057356@hanyang.ac.kr>
2021-04-23 22:06:32 +0900

    Project01

---
+++ project01/xv6-public/queue.h
@@ -0,0 +1,32 @@
+#include "param.h"
+
+#ifndef QUEUE
+#define QUEUE
+
+struct node{
+  struct proc *proc;
+  struct node *next;
+};
+
+struct queue{
+  struct node *front;
+  struct node *back;
+  struct node *cur;
+  int quantum;
+  int allotment;
+  int count;
+};
+
+void
+initqueue(struct queue *q, int quant, int allot);
+
+void
+createnode(struct proc *p, struct node *n);
+
+void
+push(struct queue *q, struct node *n);
+
+struct node *
+pop(struct queue *q, struct proc *p);
+
+#endif
